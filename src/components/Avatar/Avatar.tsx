9f227a2d66af87e8cc8177fa463cc29ca87620d0
Robson Oliveira <robsonsilva.abreu@outlook.com>
2023-03-22 02:51:45 -0300

    updates

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {ImageSourcePropType} from 'react-native';
 import {Container, AvatarImage} from './styles';
+const defaultImage = require('../../assets/AppLogo.png');
 
 interface AvatarProps {
     source: string;
@@ -7,9 +9,17 @@ interface AvatarProps {
 }
 
 export function Avatar({source, size}: AvatarProps) {
+    const [imagePath, setImagePath] =
+        useState<ImageSourcePropType>(defaultImage);
+    useEffect(() => {
+        console.log(source);
+        if (source.length > 0) {
+            setImagePath({uri: source});
+        }
+    }, [source]);
     return (
         <Container size={size}>
-            <AvatarImage source={{uri: source}} />
+            <AvatarImage source={imagePath} />
         </Container>
     );
 }
